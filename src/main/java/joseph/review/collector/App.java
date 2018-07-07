f57d1ec84f18dd7b9ac4543092ba2f20761151c6
jagauthi <joseph.a.gauthier@gmail.com>
2018-07-06 22:09:46 -0500

    Parameterization

--- src/main/java/joseph/review/collector/App.java
+++ src/main/java/joseph/review/collector/App.java
@@ -1,5 +1,7 @@
 package joseph.review.collector;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -23,15 +25,21 @@ public class App {
 		
 		reviewCollectorService = new ReviewCollectorService();
 		
-		ItemResponse itemInfo = reviewCollectorService.collectReviews();
+		List<String> upcNumbers = new ArrayList<String>();
+		
+		upcNumbers.add("035000521019");
+		
+		List<ItemResponse> itemInfo = reviewCollectorService.collectMultipleReviews(upcNumbers);
 		
 		printInfo(itemInfo);
 	}
 	
-	private void printInfo(ItemResponse itemInfo) {
-		for(ItemInfo info : itemInfo.getItems()) {
-			LOGGER.log(Level.INFO, "Item name: " + info.getName());
-			LOGGER.log(Level.INFO, "Review: " + info.getCustomerRating());
+	private void printInfo(List<ItemResponse> itemInfos) {
+		for(ItemResponse itemInfo : itemInfos) {
+			for(ItemInfo info : itemInfo.getItems()) {
+				LOGGER.log(Level.INFO, "Item name: " + info.getName());
+				LOGGER.log(Level.INFO, "Review: " + info.getCustomerRating());
+			}
 		}
 	}
 
--- src/main/java/joseph/review/collector/service/ReviewCollectorService.java
+++ src/main/java/joseph/review/collector/service/ReviewCollectorService.java
@@ -1,5 +1,7 @@
 package joseph.review.collector.service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -9,16 +11,36 @@ import joseph.review.collector.util.RestTemplate;
 public class ReviewCollectorService {
 
 	private static String url1 = "http://api.walmartlabs.com/v1/items?apiKey=ct9fe8358fa33q6w84vp9v73&upc=035000521019";
+	private static String baseUrl = "http://api.walmartlabs.com/v1/items?apiKey=ct9fe8358fa33q6w84vp9v73&upc=";
 	
 	private static final Logger LOGGER = Logger.getLogger( ReviewCollectorService.class.getName() );
 	
-	public ItemResponse collectReviews() throws Exception {
+	public List<ItemResponse> collectMultipleReviews(List<String> upcNumbers) throws Exception {
+
+		List<ItemResponse> itemResponseList = new ArrayList<ItemResponse>();
+		try {
+			for(String upcNumber : upcNumbers) {
+				ItemResponse itemResponse = collectReviews(upcNumber);
+				itemResponseList.add(itemResponse);
+			}
+		}
+		catch(Exception e) {
+			String errorMsg = "Error getting responses for all the items: " + upcNumbers;
+			LOGGER.log(Level.SEVERE, errorMsg, e);
+			throw e;
+		}
+		return itemResponseList;
+	}
+	
+	public ItemResponse collectReviews(String upcNumber) throws Exception {
 
 		ItemResponse itemResponse = new ItemResponse();
 		RestTemplate restTemplate = new RestTemplate();
 		
+		String url = baseUrl + upcNumber;
+		
 		try {
-			itemResponse = restTemplate.sendGet(url1, ItemResponse.class);
+			itemResponse = restTemplate.sendGet(url, ItemResponse.class);
 		} catch (Exception e) {
 			String errorMsg = "Error getting itemRespose";
 			LOGGER.log(Level.SEVERE, errorMsg, e);
